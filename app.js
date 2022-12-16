4dd4c22eb772191b90815a5c6f9ec1526c7099d3
olotolo <olotolo@web.de>
2022-12-16 15:52:50 +0100

    Play Clips

--- app.js
+++ app.js
@@ -3,14 +3,69 @@ const client = new tmi.Client({
 });
 
 client.connect();
-
-let links = {};
+var links = [];
+var watched = [];
 
 client.on('message', (channel, tags, message, self) => {
 	if(self) return true;
+	// Check if the link is from twitch
+    if(message.startsWith("https://www.twitch.tv/")) {
+		console.log("valid twitch clip");
+		let myArray = message.split("/");
+		let myString = myArray[5];
+		myArray = myString.split("?");
+		myString = myArray[0];
 
-	console.log(`${tags['display-name']}: ${message}`);
-    if(message.startsWith("")) {
+		// Only 100 Clips in queue max
+		if(links.length > 100) {
+			return;
+		}
 
-    }
+		// Check if the clip is currently in the queue
+		var exists = false;
+		for(var i = 0; i < links.length; i++) {
+			if(links[i] == myString) {
+				exists = true;
+				return;
+			}
+		}
+		// Check if the clip has recently been watched
+		for(var i = 0; i < links.length; i++) {
+			if(watched[i] == myString) {
+				exists = true;
+				return;
+			}
+		}
+
+		if(!exists) {
+			console.log(myString);
+			links.push(myString);
+		}
+		
+
+    } else {
+		console.log("invalid twitch clip");
+	}
 });
+
+function next() {
+	if(links[0] != null) {
+		var iframe = document.getElementById("clipiframe");
+		iframe.clip = links[0];
+		iframe.src = "https://clips.twitch.tv/embed?clip=" + links[0] + "&parent=olotolo.github.io&autoplay=true";
+		watched.push(links[0]);
+		links.shift();
+		if(watched > 100) {
+			watched.push();
+		}
+	}
+	console.log("list too short");
+}
+
+/*
+document.getElementById("clipiframe").onended = function() {test()};
+
+function test() {
+	console.log("it ended");
+}
+*/
